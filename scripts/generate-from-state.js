cebee297f706d3192cc7b0e18336f108d591cbc1
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-08-10 20:32:25 +0200

    feat: add automatic website regenerator action

--- scripts/generate-from-state.js
+++ scripts/generate-from-state.js
@@ -3,9 +3,12 @@ import {createPageFile, loadState, loadData} from "./utils.js";
 const data = loadData();
 const state = loadState();
 
-if (state !== null){
+if (state.length !== 0){
     const item = state[state.length - 1];
     const dataSelected = data.filter(obj => obj.name === item)[0];
 
     createPageFile(dataSelected);
+} else {
+    console.error("data/state.js is not present. Can't regenerate website.");
+    process.exit(1);
 }
